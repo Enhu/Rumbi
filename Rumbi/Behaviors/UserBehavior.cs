9c5a5c7680fec1b21aa10fa7e1dde04d4378fb58
Enhu <enhunano@gmail.com>
2023-01-24 03:31:11 -0300

    Finish user join and leave embeds

--- Rumbi/Behaviors/UserBehavior.cs
+++ Rumbi/Behaviors/UserBehavior.cs
@@ -1,9 +1,6 @@
 ﻿using Discord;
-using Discord.Interactions;
 using Discord.WebSocket;
 using Microsoft.Extensions.Configuration;
-using System.Reflection;
-using System.Reflection.Metadata;
 
 namespace Rumbi.Behaviors
 {
@@ -25,15 +22,40 @@ namespace Rumbi.Behaviors
             _client.PresenceUpdated += HandleUserPresenceUpdated;
         }
 
+        private async Task HandleUserJoined(SocketGuildUser user)
+        {
+            var embed = new EmbedBuilder();
+
+            ulong logChannelId = _configuration.GetValue<ulong>("Channels:LoggingChannelId");
+            var rumbiVersion = _configuration["Version"];
+
+            embed.WithAuthor($"{user.Username}#{user.Discriminator}", user.GetAvatarUrl())
+                .WithColor(0, 135, 245)
+                .WithFooter($"ID: {user.Id} | Rumbi {rumbiVersion}", user.GetAvatarUrl())
+                .WithTimestamp(DateTime.Now)
+                .WithDescription($"Account created: <t:{user.CreatedAt.ToUnixTimeSeconds()}:R>")
+                .WithTitle("Member Joined");
+
+            var logChannel = _client.GetChannel(logChannelId) as SocketTextChannel;
+            await logChannel.SendMessageAsync(embed: embed.Build());
+        }
         private async Task HandleUserLeft(SocketGuild guild, SocketUser user)
         {
-            var modsChannel = _client.GetChannel(_configuration.GetValue<ulong>("ModChannelId")) as SocketTextChannel;
-            await modsChannel.SendMessageAsync("User: username: " + user.Username + " left.");
-        }
-        private async Task HandleUserJoined(SocketGuildUser arg)
-        {
-            var modsChannel = _client.GetChannel(_configuration.GetValue<ulong>("ModChannelId")) as SocketTextChannel;
-            await modsChannel.SendMessageAsync("User: username: " + arg.Username + " joined.");
+            var embed = new EmbedBuilder();
+
+            ulong logChannelId = _configuration.GetValue<ulong>("Channels:LoggingChannelId");
+            var rumbiVersion = _configuration["Version"];
+
+            embed.WithAuthor($"{user.Username}#{user.Discriminator}", user.GetAvatarUrl())
+                .WithColor(0, 135, 245)
+                .WithFooter($"ID: {user.Id} | Rumbi {rumbiVersion}", user.GetAvatarUrl())
+                .WithTimestamp(DateTime.Now)
+                .WithDescription($"Account created: <t:{user.CreatedAt.ToUnixTimeSeconds()}:R>")
+                .WithTitle("Member Left");
+
+
+            var logChannel = _client.GetChannel(logChannelId) as SocketTextChannel;
+            await logChannel.SendMessageAsync(embed: embed.Build());
         }
 
         private async Task HandleUserPresenceUpdated(SocketUser user, SocketPresence oldPresence, SocketPresence newPresence)
