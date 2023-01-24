1d73f43cf924559cc66c302041fba96dc5f59d4a
Enhu <enhunano@gmail.com>
2023-01-24 19:13:50 -0300

    add error handling for color application

--- Rumbi/Modules/CommandsModule.cs
+++ Rumbi/Modules/CommandsModule.cs
@@ -1,5 +1,8 @@
-﻿using Discord.Interactions;
+﻿using Discord;
+using Discord.Interactions;
 using Rumbi.Services;
+using Serilog;
+using System.Drawing;
 
 namespace Rumbi.Modules
 {
@@ -21,9 +24,39 @@ namespace Rumbi.Modules
             => await RespondAsync(text: $":ping_pong: It took me {Context.Client.Latency}ms to respond to you!", ephemeral: true);
 
         [SlashCommand("colorme", "Add yourself a server color!.")]
-        public async Task ColorUser()
+        public async Task ColorUser(string hex)
         {
-            throw new NotImplementedException();
+            var color = new System.Drawing.Color();
+
+            try
+            {
+                if (!hex.StartsWith("#"))
+                    hex = string.Format("#{0}", hex);
+                color = ColorTranslator.FromHtml(hex);
+            }
+            catch (Exception)
+            {
+                await RespondAsync(text: "Invalid color code.", ephemeral: true);
+                return;
+            }
+
+            try
+            {
+                var role = await Context.Guild.CreateRoleAsync(Context.User.Username);
+
+                Discord.Color dcolor = new Discord.Color(color.R, color.G, color.B);
+
+                await role.ModifyAsync(x => x.Color = dcolor);
+
+                await Context.Guild.Users.FirstOrDefault(x => x.Id == Context.User.Id).AddRoleAsync(role);
+
+                await RespondAsync(text: "Your pretty color was applied!", ephemeral: true);
+            }
+            catch (Exception e)
+            {
+                await RespondAsync(text: $"An error ocurred. Reach an admin for more information.", ephemeral: true);
+                Log.Error(e.InnerException, e.Message);
+            }
         }
     }
 }
