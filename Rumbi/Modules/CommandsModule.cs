6e1ff510d5f5ea599a56a12fd95b6d4ed4cc70bc
Enhu <enhunano@gmail.com>
2023-01-25 06:28:07 -0300

    Clean up command module, remove WSL from launchSettings

--- Rumbi/Modules/CommandsModule.cs
+++ Rumbi/Modules/CommandsModule.cs
@@ -15,10 +15,6 @@ namespace Rumbi.Modules
             _handler = handler;
         }
 
-        [SlashCommand("echo", "Repeat the input")]
-        public async Task Echo(string echo, [Summary(description: "mention the user")] bool mention = false)
-            => await RespondAsync(echo + (mention ? Context.User.Mention : string.Empty));
-
         [SlashCommand("ping", "Pings the bot and returns its latency.")]
         public async Task GreetUserAsync()
             => await RespondAsync(text: $":ping_pong: It took me {Context.Client.Latency}ms to respond to you!", ephemeral: true);
