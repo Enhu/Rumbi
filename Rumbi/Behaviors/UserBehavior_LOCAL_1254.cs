c4564c6244a945f3d9ec83697901bf023d00e0ba
Enhu <enhunano@gmail.com>
2023-01-26 02:22:23 -0300

    Merge

---
+++ Rumbi/Behaviors/UserBehavior_LOCAL_1254.cs
@@ -0,0 +1,80 @@
+﻿using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Rumbi.Data.Config;
+using Serilog;
+
+namespace Rumbi.Behaviors
+{
+    public class UserBehavior
+    {
+        private readonly DiscordSocketClient _client;
+
+        public UserBehavior(DiscordSocketClient client)
+        {
+            _client = client;
+        }
+
+        public void Initialize()
+        {
+            _client.UserJoined += HandleUserJoined;
+            _client.UserLeft += HandleUserLeft;
+            _client.PresenceUpdated += HandleUserPresenceUpdated;
+        }
+
+        private async Task HandleUserJoined(SocketGuildUser user)
+        {
+            var embed = new EmbedBuilder();
+
+            ulong logChannelId = RumbiConfig.Configuration.LoggingChannel;
+            var rumbiVersion = RumbiConfig.Configuration.Version;
+
+            embed.WithAuthor($"{user.Username}#{user.Discriminator}", user.GetAvatarUrl())
+                .WithColor(0, 135, 245)
+                .WithFooter($"ID: {user.Id} | Rumbi {rumbiVersion}", user.GetAvatarUrl())
+                .WithTimestamp(DateTime.Now)
+                .WithDescription($"Account created: <t:{user.CreatedAt.ToUnixTimeSeconds()}:R>")
+                .WithTitle("Member Joined");
+
+            var logChannel = _client.GetChannel(logChannelId) as SocketTextChannel;
+            await logChannel.SendMessageAsync(embed: embed.Build());
+        }
+        private async Task HandleUserLeft(SocketGuild guild, SocketUser user)
+        {
+            var embed = new EmbedBuilder();
+
+            ulong logChannelId = RumbiConfig.Configuration.LoggingChannel;
+            var rumbiVersion = RumbiConfig.Configuration.Version;
+
+            embed.WithAuthor($"{user.Username}#{user.Discriminator}", user.GetAvatarUrl())
+                .WithColor(0, 135, 245)
+                .WithFooter($"ID: {user.Id} | Rumbi {rumbiVersion}", user.GetAvatarUrl())
+                .WithTimestamp(DateTime.Now)
+                .WithDescription($"Account created: <t:{user.CreatedAt.ToUnixTimeSeconds()}:R>")
+                .WithTitle("Member Left");
+
+
+            var logChannel = _client.GetChannel(logChannelId) as SocketTextChannel;
+            await logChannel.SendMessageAsync(embed: embed.Build());
+        }
+
+        private async Task HandleUserPresenceUpdated(SocketUser user, SocketPresence oldPresence, SocketPresence newPresence)
+        {
+            if(oldPresence.Activities.Any(x => x.Type == ActivityType.Streaming && x.Name == "A Hat in Time"))
+            {
+                var guild = _client.GetGuild(RumbiConfig.Configuration.Guild);
+                var streamingRole = guild.GetRole(RumbiConfig.Configuration.Streaming);
+                var guildUser = guild.GetUser(user.Id);
+                await guildUser.RemoveRoleAsync(streamingRole);
+            }
+
+            if(newPresence.Activities.Any(x => x.Type == ActivityType.Streaming && x.Name == "A Hat in Time"))
+            {
+                var guild = _client.GetGuild(RumbiConfig.Configuration.Guild);
+                var streamingRole = guild.GetRole(RumbiConfig.Configuration.Streaming);
+                var guildUser = guild.GetUser(user.Id);
+                await guildUser.AddRoleAsync(streamingRole);
+            }
+        }
+    }
+}
