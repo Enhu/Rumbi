a8b2fa7beef367ee7f29aa92d7cee77fc193dde2
Enhu <enhunano@gmail.com>
2023-06-20 22:23:26 -0300

    update database

---
+++ Rumbi.Data/Migrations/20230621011228_UpdateNames.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Rumbi.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateNames : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Config");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_GuildUsers",
+                table: "GuildUsers");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_GuildRoles",
+                table: "GuildRoles");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_GuildChannels",
+                table: "GuildChannels");
+
+            migrationBuilder.RenameTable(
+                name: "GuildUsers",
+                newName: "Users");
+
+            migrationBuilder.RenameTable(
+                name: "GuildRoles",
+                newName: "Roles");
+
+            migrationBuilder.RenameTable(
+                name: "GuildChannels",
+                newName: "Channels");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Roles",
+                table: "Roles",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Channels",
+                table: "Channels",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Roles",
+                table: "Roles");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Channels",
+                table: "Channels");
+
+            migrationBuilder.RenameTable(
+                name: "Users",
+                newName: "GuildUsers");
+
+            migrationBuilder.RenameTable(
+                name: "Roles",
+                newName: "GuildRoles");
+
+            migrationBuilder.RenameTable(
+                name: "Channels",
+                newName: "GuildChannels");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_GuildUsers",
+                table: "GuildUsers",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_GuildRoles",
+                table: "GuildRoles",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_GuildChannels",
+                table: "GuildChannels",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "Config",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    TwitchAccessToken = table.Column<int>(type: "integer", nullable: false),
+                    TwitchTokenExpirationDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Config", x => x.Id);
+                });
+        }
+    }
+}
--- Rumbi.Data/RumbiContext.cs
+++ Rumbi.Data/RumbiContext.cs
@@ -5,44 +5,32 @@ namespace Rumbi.Data
 {
     public class RumbiContext : DbContext
     {
-        public RumbiContext(DbContextOptions<RumbiContext> options) : base(options)
-        {
-
-        }
+        public RumbiContext(DbContextOptions<RumbiContext> options)
+            : base(options) { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Models.Config>()
-                .Property(p => p.Id)
-                .ValueGeneratedOnAdd();
+            modelBuilder.Entity<Meme>().Property(p => p.Id).ValueGeneratedOnAdd();
 
-            modelBuilder.Entity<Meme>()
-                .Property(p => p.Id)
-                .ValueGeneratedOnAdd();
+            modelBuilder.Entity<AnnouncementAttachment>().Property(p => p.Id).ValueGeneratedOnAdd();
 
-            modelBuilder.Entity<AnnouncementAttachment>()
-                .Property(p => p.Id)
-                .ValueGeneratedOnAdd();
+            modelBuilder.Entity<TemporalFile>().Property(p => p.Id).ValueGeneratedOnAdd();
 
-            modelBuilder.Entity<TemporalFile>()
-                .Property(p => p.Id)
-                .ValueGeneratedOnAdd();
-
-            modelBuilder.Entity<Announcement>()
+            modelBuilder
+                .Entity<Announcement>()
                 .HasOne(a => a.Attachment)
                 .WithOne(x => x.Announcement)
                 .HasForeignKey<AnnouncementAttachment>(y => y.AnnouncementId)
                 .OnDelete(DeleteBehavior.Cascade);
         }
 
-        public DbSet<User> GuildUsers { get; set; } = null!;
-        public DbSet<Channel> GuildChannels { get; set; } = null!;
-        public DbSet<Role> GuildRoles { get; set; } = null!;
+        public DbSet<User> Users { get; set; } = null!;
+        public DbSet<Channel> Channels { get; set; } = null!;
+        public DbSet<Role> Roles { get; set; } = null!;
         public DbSet<Meme> Memes { get; set; } = null!;
         public DbSet<Announcement> Announcements { get; set; } = null!;
         public DbSet<AnnouncementAttachment> AnnouncementAttachments { get; set; } = null!;
         public DbSet<TemporalFile> TemporalFiles { get; set; } = null!;
         public DbSet<Poll> Polls { get; set; } = null!;
-        public DbSet<Models.Config> Config { get; set; } = null!;
     }
 }
